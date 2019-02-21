100996f02fde978284c0ac494419782942456d4e
Michael Bausano <michael@joinpouch.com>
2019-02-21 15:47:32 +0000

    Tests pending

---
+++ test/spec/App.spec.ts
@@ -0,0 +1,10 @@
+
+describe('App', () => {
+
+  it('boots the listener')
+
+  it('sends messages after receiving one from the listener')
+
+  it('does not send messages with timestamp older than last rendered message')
+
+})
